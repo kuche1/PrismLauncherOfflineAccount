376c2e3d3b1a2a87fb80b6d63bfb0006fef12e84
kuche1 <znamsiparolata4esno@abv.bg>
2025-04-12 01:24:23 +0300

    update

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@
 
 	BUILD_DIR="$HERE/build"
 
+	LOW_PRIORITY='nice -n 19'
+
 	function main(){
 		rm -rf "$BUILD_DIR"
 		mkdir "$BUILD_DIR"
@@ -15,26 +17,28 @@
 		cd "$BUILD_DIR"
 
 		{
-			# latest development branch
-			# last tested 2025.04.08: commit 734db2bedcb9ebd12667031e423d16fd1118a39f
-			#git clone --recursive https://github.com/PrismLauncher/PrismLauncher.git
+			# # last tested 2025.04.08: commit 734db2bedcb9ebd12667031e423d16fd1118a39f
+			# echo 'Cloning latest development branch'
+			# git clone --recursive https://github.com/PrismLauncher/PrismLauncher.git
 
-			# 9.x
 			# last tested 2025.04.11: commit 385bb500b9195765a6e6fb5c81855b24eb48bf91
+			echo 'Cloning 9.x branch...'
 			git clone --recursive -b release-9.x https://github.com/PrismLauncher/PrismLauncher.git
 		}
 
 		cd PrismLauncher
 
+		echo 'Applying patches...'
 		git apply "$HERE"/patches/???-*
 
 		git add .
 		# not needed but it makes it easier for me to add new patches
 
-		cmake -S . -B build -G Ninja -DCMAKE_INSTALL_PREFIX=install
-		cmake --build build
-		cmake --install build
-		cmake --install build --component portable
+		echo 'Building...'
+		$LOW_PRIORITY cmake -S . -B build -G Ninja -DCMAKE_INSTALL_PREFIX=install
+		$LOW_PRIORITY cmake --build build
+		$LOW_PRIORITY cmake --install build
+		$LOW_PRIORITY cmake --install build --component portable
 
 		echo
 		echo "Portable launcher located at: $(pwd)/install"
@@ -43,4 +47,3 @@
 
 	main
 }
-
